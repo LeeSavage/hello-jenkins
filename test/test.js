ec46995f73a6cb51a06e99dd476c2db9707fc610
temporary <ls42936m1@yahoo.com>
2015-06-08 16:44:15 +0100

    update test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins', done);
+    request(app).get('/').expect('hey what\'s going on?', done);
   });
-});
+);
