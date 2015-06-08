6e49a8a97d73be8c3955fa5c3661e49ac0c8df95
temporary <ls42936m1@yahoo.com>
2015-06-09 00:28:31 +0100

    whoah

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hey whats going on?', done);
+    request(app).get('/').expect('whoah', done);
   });
 });
